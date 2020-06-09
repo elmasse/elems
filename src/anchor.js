aaef0a1d4ca8620b3a98e3f0c5d861e8688499af
Max Fierro <elmasse@gmail.com>
2020-06-09 09:42:32 -0300

    Make Anchor a Component. Closes #3/

--- src/anchor.js
+++ src/anchor.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const Anchor = ({ children, ...props }) => {
-  return (
-    <a {...props}>
-      {children}
-      <style jsx>
-        {`
-        a {
-          color: var(--action-color);
-        }
-      `}
-      </style>
-    </a>
-  )
+export class Anchor extends Component {
+  render () {
+    const { children, ...props } = this.props
+    return (
+      <a {...props}>
+        {children}
+        <style jsx>
+          {`
+          a {
+            color: var(--action-color);
+          }
+        `}
+        </style>
+      </a>
+    )
+  }
 }
 
 export const A = Anchor
